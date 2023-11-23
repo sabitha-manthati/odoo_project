17042aea8ade03a5495fccaea73127e18bdb45b5
sabitha_manthati <sabithamanthati09@gmail.com>
2023-11-23 23:14:59 +0530

    record rule added for sales team, leader should able to see their team members data in crm

---
+++ crm_inherited/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See COPYRIGHT & LICENSE files for full copyright and licensing details.
+
+{
+    'name': 'CRM Lead Extended',
+    'version': '16.0.0.1',
+    'category': 'Project',
+    'author': 'sabitha',
+    'summary': 'CRM. ',
+    'website': 'www.rdp.in',
+    'sequence': '10',
+    'description': """
+     
+    """,
+    'depends': ['base','crm','sale'],
+    'data': [
+
+        'security/security.xml',
+       
+
+    ],
+
+    # 'license': 'OPL-1',
+    # 'application': True,
+    'installable': True,
+
+
+}
---
+++ crm_inherited/models/crm_lead.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See COPYRIGHT & LICENSE files for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class CRMLead(models.Model):
+
+    _inherit = 'crm.lead'
+
+
+
+
+
+
+
+
